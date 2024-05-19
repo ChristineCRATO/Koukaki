a1e7f6e3dba80a019dbd08a7d025b59518f5ee76
GitPixel <gitpixel54@gmail.com>
2024-05-19 23:37:37 +0200

    add oscars section+css+scss

--- wp-content/themes/foce-child/functions.php
+++ wp-content/themes/foce-child/functions.php
@@ -2,6 +2,8 @@
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
 function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+    // Loading child theme style
+    wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/css-sass/style.css', array(), time());
 }
 
 // Get customizer options form parent theme
