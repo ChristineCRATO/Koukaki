ade86d28f92f17eda4f99518f29df433d2c112a8
GitPixel <gitpixel54@gmail.com>
2024-06-22 16:43:53 +0200

    update responsive

--- wp-content/themes/foce-child/front-page.php
+++ wp-content/themes/foce-child/front-page.php
@@ -11,7 +11,7 @@ get_header();
             <img class="bannerImg" src="<?php echo get_template_directory_uri() . '/assets/images/logo.png'; ?> " alt="logo Fleurs d'oranger & chats errants">
         </section>
         <section id="story" class="story">
-            <h2><span class="TitleFade">L'histoire</span></h2>
+            <h2><span class="TitleFade storyTitle">L'histoire</span></h2>
             <article id="" class="story__article">
                 <p><?php echo get_theme_mod('story'); ?></p>
             </article>
@@ -37,8 +37,8 @@ get_header();
             </article>
         </section>
 
-        <section id="studio">
-            <h2><span class="TitleFade">Studio Koukaki</span></h2>
+        <section id="studio" class="titleStudio">
+            <h2><span class="TitleFade studioTitlte">Studio Koukaki</span></h2>
             <div>
                 <p>Acteur majeur de l’animation, Koukaki est un studio intégré fondé en 2012 qui créé, produit et distribue des programmes originaux dans plus de 190 pays pour les enfants et les adultes. Nous avons deux sections en activité : le long métrage et le court métrage. Nous développons des films fantastiques, principalement autour de la culture de notre pays natal, le Japon.</p>
                 <p>Avec une créativité et une capacité d’innovation mondialement reconnues, une expertise éditoriale et commerciale à la pointe de son industrie, le Studio Koukaki se positionne comme un acteur incontournable dans un marché en forte croissance. Koukaki construit chaque année de véritables succès et capitalise sur de puissantes marques historiques. Cette année, il vous présente “Fleurs d’oranger et chats errants”.</p>
