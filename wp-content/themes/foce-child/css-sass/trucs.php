6a0bd6c65fdbb6cccaec0903a9912f069e53892a
GitPixel <gitpixel54@gmail.com>
2024-06-25 15:39:54 +0200

    update responsive

---
+++ wp-content/themes/foce-child/css-sass/trucs.php
@@ -0,0 +1,30 @@
+@media screen and (min-width: 320px) and (max-width: 767px) {
+    .main-navigation {
+      max-width: 767px; ;
+      display: flex;
+      align-items: center;
+    }
+  }
+  @media (max-width: 768px) {
+    .main-navigation {
+      max-width: 768px;
+      height: 80px;
+      display: flex;
+      align-items: center;
+    }
+  @media (max-width: 1024px) {
+    .main-navigation {
+      max-width: 1024px;
+      height: 80px;
+    }
+  }
+  @media (max-width: 1200px) {
+    .main-navigation {
+      max-width: 1200px;
+    }
+  }
+  @media (max-width: 1500px) {
+    .main-navigation {
+      max-width: 1500px;
+    }
+  }
