2f411dc504dd2f3b4ba70528f96428bdd05620a5
GitPixel <gitpixel54@gmail.com>
2024-06-25 15:55:58 +0200

    responsive update

--- wp-content/themes/CRATO_Christine_1_theme-enfant_05-2024/templates/oscars.php
+++
@@ -1,6 +0,0 @@
-<section id="oscars" class="oscars">
-    <div class="blocImgText">
-        <h3 class="txt-oscars">Fleurs d'oranger & Chats errants <br> est nominé aux Oscars Short <br> Film Animated de 2022 ! </h3>
-    </div>
-    <img class="logoscars" src="<?php echo get_theme_file_uri() . '/assets/images/logoscars.png'; ?>" alt="Logo des Oscars 2022">
-</section>
